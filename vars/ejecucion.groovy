8a43985204f947d1a33173e1f1b02935cba951a0
Juan Hurtado <juan.hurtado@equiliza.com>
2022-01-27 21:24:06 -0300

    Actuazar Parametro Stage

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -35,7 +35,8 @@ def call(){
 		post {
 			success {
                 		script {
-					if (STAGE.trim() != '') {
+					println "Ejecución stage ${env.STAGE}"
+					if (env.STAGE != '') {
 						slackSend color: 'good', message: "[${env.BUILD_USER}][${env.USUARIO}][${env.JOB_NAME}][${params.buildTool}] Ejecución Exitosa!"
 					} else {
 						slackSend color: 'danger', message: "[${env.BUILD_USER}][${env.USUARIO}][${env.JOB_NAME}][${params.buildTool}] Ejecución fallida en stage ${params.stage}"
