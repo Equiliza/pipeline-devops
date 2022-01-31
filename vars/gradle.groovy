37a82623ad431db60f36f55194eb46716999fa68
Juan Hurtado <juan.hurtado@equiliza.com>
2022-01-31 19:13:57 -0300

    Actuazar CICD

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -7,57 +7,88 @@
 def call(String PipeLineType){
 
 	figlet PipeLineType
-  
-        stage('BuildTestJar') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    bat "./gradlew clean build"                       
+
+	if (PipeLineType == 'CI') {
+        	stage('BuildTestJar') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "./gradlew clean build"                       
+			}
+        	}
+        	stage('Sonar') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	def scannerHome = tool 'sonar-scanner';
+                    	withSonarQubeEnv('sonar-server') {
+                    	bat "C:/Users/Patric~1/.jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar-scanner/bin/sonar-scanner.bat -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build"
+                    	}
+			}
+        	}
+        	stage('Run') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "start /min gradlew bootRun &"
+                    	sleep 20
+			}
+        	}
+        	stage('TestApp') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "start chrome http://localhost:8082/rest/mscovid/test?msg=testing"
+			}
+        	}
+		stage('NexusUpload') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+		    	nexusPublisher nexusInstanceId: 'test-nexus', nexusRepositoryId: 'test.nexus',
+		    	packages: [[$class: 'MavenPackage',
+				mavenAssetList: [[classifier: '',
+				extension: '',
+				filePath: 'C:/Users/Patric~1/Desktop/Ejercicio/ejemplo-gradle/build/libs/DevOpsUsach2020-0.0.1.jar']],
+				mavenCoordinate: [artifactId: 'DevOpsUsach2020',
+				groupId: 'com.devopsusach2020',
+				packaging: 'jar',
+				version: '0.0.1']
+				]]
+			}
 		}
-        }
-        stage('Sonar') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    def scannerHome = tool 'sonar-scanner';
-                    withSonarQubeEnv('sonar-server') {
-                    bat "C:/Users/Patric~1/.jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar-scanner/bin/sonar-scanner.bat -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build"
-                    }
-		}
-        }
-        stage('Run') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    bat "start /min gradlew bootRun &"
-                    sleep 20
-		}
-        }
-        stage('TestApp') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    bat "start chrome http://localhost:8082/rest/mscovid/test?msg=testing"
-		}
-        }
-	stage('NexusUpload') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-		    nexusPublisher nexusInstanceId: 'test-nexus', nexusRepositoryId: 'test.nexus',
-		    packages: [[$class: 'MavenPackage',
-			mavenAssetList: [[classifier: '',
-			extension: '',
-			filePath: 'C:/Users/Patric~1/Desktop/Ejercicio/ejemplo-gradle/build/libs/DevOpsUsach2020-0.0.1.jar']],
-			mavenCoordinate: [artifactId: 'DevOpsUsach2020',
-			groupId: 'com.devopsusach2020',
-			packaging: 'jar',
-			version: '0.0.1']
-			]]
+	} else {
+        	stage('NexusDownload') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "curl -X GET -u admin:Pelusa50# http://localhost:8081/repository/test.nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"  
+                    	bat "dir" 
+			}
+        	}
+        	stage('Run') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "start /min gradlew bootRun &"
+                    	sleep 20
+			}
+        	}
+        	stage('TestApp') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "start chrome http://localhost:8082/rest/mscovid/test?msg=testing"
+			}
+        	}
+		stage('NexusUpload') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+		    	nexusPublisher nexusInstanceId: 'test-nexus', nexusRepositoryId: 'test.nexus',
+		    	packages: [[$class: 'MavenPackage',
+				mavenAssetList: [[classifier: '',
+				extension: '',
+				filePath: 'C:/Users/Patric~1/Desktop/Ejercicio/ejemplo-gradle/build/libs/DevOpsUsach2020-0.0.1.jar']],
+				mavenCoordinate: [artifactId: 'DevOpsUsach2020',
+				groupId: 'com.devopsusach2020',
+				packaging: 'jar',
+				version: '1.0.0']
+				]]
+			}
 		}
 	}
-        stage('NexusDownload') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    bat "curl -X GET -u admin:Pelusa50# http://localhost:8081/repository/test.nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"  
-                    bat "dir" 
-		}
-        }
 
 }
 
--- vars/maven.groovy
+++ vars/maven.groovy
@@ -7,69 +7,100 @@
 def call(String PipeLineType){
 
 	figlet PipeLineType
-  
-        stage('Build') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    bat "./mvnw.cmd clean compile -e"                       
+
+	if (PipeLineType == 'CI') {
+        	stage('Build') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "./mvnw.cmd clean compile -e"                       
+			}
+        	}
+        	stage('Test') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "./mvnw.cmd clean test -e"                       
+			}
+        	}
+        	stage('Jar') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "./mvnw.cmd clean package -e"                       
+			}
+        	}
+        	stage('Sonar') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	def scannerHome = tool 'sonar-scanner';
+                    	withSonarQubeEnv('sonar-server') {
+                    	bat "C:/Users/Patric~1/.jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar-scanner/bin/sonar-scanner.bat -Dsonar.projectKey=ejemplo-maven-new2 -Dsonar.sources=src -Dsonar.java.binaries=build"
+                    	}
+			}
+        	}
+        	stage('Run') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "start /min mvnw spring-boot:run &"
+                    	sleep 20
+			}
+        	}
+        	stage('TestApp') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "start chrome http://localhost:8082/rest/mscovid/test?msg=testing"
+			}
+        	}
+		stage('NexusUpload') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+		    	nexusPublisher nexusInstanceId: 'test-nexus', nexusRepositoryId: 'test.nexus',
+		    	packages: [[$class: 'MavenPackage',
+				mavenAssetList: [[classifier: '',
+				extension: '',
+				filePath: 'C:/Users/Patric~1/Desktop/Ejercicio/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']],
+				mavenCoordinate: [artifactId: 'DevOpsUsach2020',
+				groupId: 'com.devopsusach2020',
+				packaging: 'jar',
+				version: '0.0.1']
+				]]
+			}
 		}
-        }
-        stage('Test') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    bat "./mvnw.cmd clean test -e"                       
-		}
-        }
-        stage('Jar') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    bat "./mvnw.cmd clean package -e"                       
-		}
-        }
-        stage('Sonar') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    def scannerHome = tool 'sonar-scanner';
-                    withSonarQubeEnv('sonar-server') {
-                    bat "C:/Users/Patric~1/.jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar-scanner/bin/sonar-scanner.bat -Dsonar.projectKey=ejemplo-maven-new2 -Dsonar.sources=src -Dsonar.java.binaries=build"
-                    }
-		}
-        }
-        stage('Run') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    bat "start /min mvnw spring-boot:run &"
-                    sleep 20
-		}
-        }
-        stage('TestApp') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    bat "start chrome http://localhost:8082/rest/mscovid/test?msg=testing"
-		}
-        }
-	stage('NexusUpload') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-		    nexusPublisher nexusInstanceId: 'test-nexus', nexusRepositoryId: 'test.nexus',
-		    packages: [[$class: 'MavenPackage',
-			mavenAssetList: [[classifier: '',
-			extension: '',
-			filePath: 'C:/Users/Patric~1/Desktop/Ejercicio/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']],
-			mavenCoordinate: [artifactId: 'DevOpsUsach2020',
-			groupId: 'com.devopsusach2020',
-			packaging: 'jar',
-			version: '0.0.1']
-			]]
+	} else {
+        	stage('NexusDownload') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "curl -X GET -u admin:Pelusa50# http://localhost:8081/repository/test.nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"  
+                    	bat "dir" 
+			}
+        	}
+        	stage('Run') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "start /min mvnw spring-boot:run &"
+                    	sleep 20
+			}
+        	}
+        	stage('TestApp') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+                    	bat "start chrome http://localhost:8082/rest/mscovid/test?msg=testing"
+			}
+        	}
+		stage('NexusUpload') {
+			if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
+		    	env.STAGE=env.STAGE_NAME
+		    	nexusPublisher nexusInstanceId: 'test-nexus', nexusRepositoryId: 'test.nexus',
+		    	packages: [[$class: 'MavenPackage',
+				mavenAssetList: [[classifier: '',
+				extension: '',
+				filePath: 'C:/Users/Patric~1/Desktop/Ejercicio/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']],
+				mavenCoordinate: [artifactId: 'DevOpsUsach2020',
+				groupId: 'com.devopsusach2020',
+				packaging: 'jar',
+				version: '1.0.0']
+				]]
+			}
 		}
 	}
-        stage('NexusDownload') {
-		if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'Todo') {
-		    env.STAGE=env.STAGE_NAME
-                    bat "curl -X GET -u admin:Pelusa50# http://localhost:8081/repository/test.nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"  
-                    bat "dir" 
-		}
-        }
 
 }
 
