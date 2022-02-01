7afc9ab769006016ea193ddc7038dc7ad7621391
Juan Hurtado <juan.hurtado@equiliza.com>
2022-01-31 21:10:10 -0300

    Actuazar CICD

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -31,7 +31,7 @@ def call(String PipeLineType){
 			figlet "Stage: ${env.STAGE}"
                     	def scannerHome = tool 'sonar-scanner';
                     	withSonarQubeEnv('sonar-server') {
-                    	bat "C:/Users/Patric~1/.jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar-scanner/bin/sonar-scanner.bat -Dsonar.projectKey=pipeline-devops -Dsonar.sources=src -Dsonar.java.binaries=build"
+                    	bat "C:/Users/Patric~1/.jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar-scanner/bin/sonar-scanner.bat -Dsonar.projectKey=ejemplo-maven-new2 -Dsonar.sources=src -Dsonar.java.binaries=build"
                     	}
         	}
         	stage('Run') {
